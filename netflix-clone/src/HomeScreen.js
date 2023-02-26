d97631844f0b28cfe5d08a3ab645218b26d8fc1d
Mark Shin <contact.mshin@gmail.com>
2023-02-26 18:05:27 -0500

    creating react/redux app

---
+++ netflix-clone/src/HomeScreen.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import "./HomeScreen.css";
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+        {/* Navbar */}
+        <Nav/>
+        {/* Banner */}
+
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        {/* Rows */}
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJd2gXuCOjEV7_Vt5XRxO7JJyzfeZSA8U",
+    authDomain: "marknetflixclone.firebaseapp.com",
+    projectId: "marknetflixclone",
+    storageBucket: "marknetflixclone.appspot.com",
+    messagingSenderId: "320779896706",
+    appId: "1:320779896706:web:f1706bea1d549dba3e319b"
+};
