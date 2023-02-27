7522d9d8b72e611d0b8129aa3ba74b62cd149cc4
Mark Shin <contact.mshin@gmail.com>
2023-02-26 22:38:54 -0500

    styling changes

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -20,7 +20,7 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav__black"}`}>
         <div className="nav__contents">
-            <img className='nav__logo' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+            <img className='nav__logo' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="" />
             <img className='nav__avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
         </div>
     </div>
